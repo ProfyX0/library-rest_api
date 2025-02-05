4b78afcf0974f032e6b5f308f89a7c13fd5f00ce
sokolja7 <sokolja@fit.cvut.cz>
2025-02-05 20:20:20 +0100

    Added forgotten tests

--- test_main.py
+++ test_main.py
@@ -1,6 +1,8 @@
 from fastapi.testclient import TestClient
+from data_import import *
 
 from main import app
+
 client = TestClient(app)
 
 def test_read_main():
@@ -32,3 +34,21 @@ def test_author_report():
 def test_invalid_url():
     response = client.get("/invalid")
     assert response.status_code == 404
+
+def test_add_book():
+    book = Book(title="Skripta MA2", author="Tomas Kalvoda")
+    dict_books = {}
+    dict_books = add_book_to_dict(dict_books, book)
+    assert dict_books == {
+        "Tomas Kalvoda": {book}
+    }
+
+def test_add_same_book_twice():
+    book = Book(title="Skripta MA2", author="Tomas Kalvoda")
+    dict_books = {}
+    dict_books = add_book_to_dict(dict_books, book)
+    dict_books = add_book_to_dict(dict_books, book)
+    assert dict_books == {
+        "Tomas Kalvoda": {book}
+    }
+    assert book.count == 2
